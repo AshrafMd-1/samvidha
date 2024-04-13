ca986d825938fcc389b97eed27aad80b27085742
Ashraf Mohammed <ashrafmd0506@gmail.com>
2024-04-13 13:49:32 +0530

    add about page

--- menu.py
+++ menu.py
@@ -6,11 +6,13 @@ def authenticated_menu():
     st.sidebar.page_link("pages/attendance.py", label="Attendance")
     st.sidebar.page_link("pages/pat.py", label="Pat")
     st.sidebar.page_link("pages/biometric.py", label="Biometric")
+    st.sidebar.page_link("pages/about.py", label="About")
     st.sidebar.page_link("app.py", label="Logout")
 
 
 def unauthenticated_menu():
     st.sidebar.page_link("app.py", label="Log in")
+    st.sidebar.page_link("pages/about.py", label="About")
 
 
 def menu():
---
+++ pages/about.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from menu import menu
+
+st.title("About")
+st.markdown("Samvidha app created by **Ashraf (Tech Lead)**")
+
+st.write("This app is created for the students of IARE to check their attendance and other details.")
+
+st.markdown("### Made by :rainbow[GDSC IARE]")
+
+st.markdown(":rainbow[Version : 1.0.0]")
+st.markdown(":rainbow[Date : 12-04-2024].")
+menu()
